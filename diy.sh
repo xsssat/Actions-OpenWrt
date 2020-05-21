ed5ff540f82fe51a47f24e3c623fd2a3e27011c4
abt12680 <50388398+abt12680@users.noreply.github.com>
2020-05-21 08:55:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,12 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
-sed -i 's/tplink-8mlzma/tplink-16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
-sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/tiny-tp-link.mk
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/tplink-8mlzma/tplink-16mlzma/g' target/linux/ar71xx/image/tiny-tp-link.mk
+#sed -i 's/tplink-8m/tplink-16m/g' target/linux/ar71xx/image/tiny-tp-link.mk
+
+
+#自定插件
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
